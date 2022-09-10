4606a6ae68e95ae025b27f72d58010a9a177fa76
Andri Anto <andrianto.ykhc@gmail.com>
2022-09-11 03:13:49 +0700

    Update botika_socket_client_example.dart

--- example/botika_socket_client_example.dart
+++ example/botika_socket_client_example.dart
@@ -1,6 +1,7 @@
 import 'package:botika_socket_client/botika_socket_client.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var socket = SocketClient('https://socket.example.com', {'token': 'example'});
+  var channel = socket.channel('test');
+  channel.on('message:create', (data) => print(data));
 }
