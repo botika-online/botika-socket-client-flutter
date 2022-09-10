b70c1240b35b846e9cc090c3ce9f1d4673af526e
Andri Anto <andrianto.ykhc@gmail.com>
2022-09-11 03:13:54 +0700

    Update botika_socket_client_test.dart

--- test/botika_socket_client_test.dart
+++ test/botika_socket_client_test.dart
@@ -1,16 +1,18 @@
 import 'package:botika_socket_client/botika_socket_client.dart';
+import 'package:socket_io_client/socket_io_client.dart' as socket_io_client;
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+    final socket =
+        SocketClient('https://socket.example.com', {'token': 'example'});
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Return Channel', () {
+      expect(socket.channel('test'), isA<socket_io_client.Socket>());
     });
   });
 }
